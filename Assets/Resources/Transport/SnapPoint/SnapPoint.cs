fa3c6cf04f398f3b231d5a8ade6a8d6b9d612ddc
Clayton Hickey <clayton.hickey@yahoo.com>
2020-04-19 02:25:57 -0400

    init

---
+++ Assets/Resources/Transport/SnapPoint/SnapPoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapPoint : MonoBehaviour
+{
+    private Player player;
+    private new MeshRenderer renderer;
+    public Material baseMaterial;
+    public Material selected1Material;
+    public Material selected2Material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        renderer = gameObject.GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool lookingAt = player.lookingAtSnapPoint == gameObject;
+        bool selected1 = player.selectedSnapPoint1 == gameObject;
+        bool selected2 = player.selectedSnapPoint2 == gameObject;
+        renderer.enabled = (lookingAt || selected1 || selected2);
+        renderer.material = selected1 ? selected1Material : (selected2 ? selected2Material : baseMaterial);
+    }
+}
---
+++ Assets/Resources/Transport/Transport.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transport : MonoBehaviour
+{
+    public GameObject core;
+    Vector2 moveAxis = new Vector2(0, 0);
+
+    void Start ()
+    {
+        core = GameObject.Instantiate(Resources.Load<GameObject>("Transport/Parts/Core/CorePrefab"), gameObject.transform);
+    }
+
+    public void SetMoveAxis(Vector2 moveAxis)
+    {
+        this.moveAxis = moveAxis;
+    }
+}
